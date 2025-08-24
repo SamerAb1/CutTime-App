544d15592dae07e2f6ca57c95a7d5d628bfe70ed
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-08-24 13:54:38 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { supabase } from "./supabase-client";
 import "./App.css";
 
